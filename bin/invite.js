80567bb1a4e438aea5aa575bf66818c94e4d0bcb
Austin King <shout@ozten.com>
2013-11-02 10:34:54 -0700

    Starting to save workflows

--- bin/invite.js
+++ bin/invite.js
@@ -12,13 +12,13 @@ if (3 !== process.argv.length) {
 var email = process.argv[2];
 
 // create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = mailer.createTransport("SMTP",{
+var smtpTransport = mailer.createTransport("SMTP", {
     host: config.smtp_hostname,
     port: config.smtp_port,
     debug: true,
     auth: {
-    	user: config.smtp_login,
-    	pass: config.smtp_password
+        user: config.smtp_login,
+        pass: config.smtp_password
     }
 });
 
@@ -27,41 +27,25 @@ var mailOptions = {
     from: "Spotlighters <team@mozillaspotlight.com>",
     to: email,
     subject: "You're in the Mozilla Spotlight!",
-    text: "Hello!\n" +
-"\n" +
-"If you had four minutes to say anything you wanted to all Mozillians, in public, what would you say?\n" +
-"\n" +
-"Welcome to Spotlight.\n" +
-"Our new web site that introduces one of us, to all the rest of us, each day.\n" +
-"\n" +
-"Spotlight randomly contacts one mozillian a day, giving them the chance to share themselves and their thoughts with the Mozilla community. After creating and sending in a four minute video (...animation, presentation, screencast, whatever), Spotlight will post it on planet.mozilla.org for everyone to appreciate.\n" +
-"\n" +
-"Will you share an idea that keeps you up at night?  Give us a tour of your hometown?  Show the community your favorite recipe? Share whatever you'd like - today the spotlight is on you!\n" +
-"\n" +
-"You've been randomly chosen today!  Learn more about Spotlight and send us your video\n" +
-"http://www.mozillaspotlight.com/invited\n" +
-"\n" +
-"Cheers,\n" +
-"\n" +
-"The Spotlight Team\n"
-}
+    text: "Hello!\n" + "\n" + "If you had four minutes to say anything you wanted to all Mozillians, in public, what would you say?\n" + "\n" + "Welcome to Spotlight.\n" + "Our new web site that introduces one of us, to all the rest of us, each day.\n" + "\n" + "Spotlight randomly contacts one mozillian a day, giving them the chance to share themselves and their thoughts with the Mozilla community. After creating and sending in a four minute video (...animation, presentation, screencast, whatever), Spotlight will post it on planet.mozilla.org for everyone to appreciate.\n" + "\n" + "Will you share an idea that keeps you up at night?  Give us a tour of your hometown?  Show the community your favorite recipe? Share whatever you'd like - today the spotlight is on you!\n" + "\n" + "You've been randomly chosen today!  Learn more about Spotlight and send us your video\n" + "http://www.mozillaspotlight.com/invited\n" + "\n" + "Cheers,\n" + "\n" + "The Spotlight Team\n"
+};
 
 console.log('Sending email', mailOptions);
 
 // send mail with defined transport object
-smtpTransport.sendMail(mailOptions, function(error, response){
+smtpTransport.sendMail(mailOptions, function(error, response) {
     console.log('smtpTransport callback', error, response);
     smtpTransport.close();
-    if(error){
-	   console.log(error);
-    }else{
+    if (error) {
+        console.log(error);
+    } else {
         console.log('message sent, saving workflow');
         var workflow = {
             email: email,
             // created is also the invited time
             created: new Date().getTime()
         };
-	    db.saveSpotlightWorkflow(email, workflow, function(err) {
+        db.saveSpotlightWorkflow(email, workflow, function(err) {
             db.close();
             if (err) {
                 console.error(err);
